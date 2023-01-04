e07a267d455a2a59543c456edee2d9e544a833f9
SUSHANT MAHESHWAR PANIGRAHI <62639871+sushantro@users.noreply.github.com>
2023-01-04 21:00:31 +0530

    count and count1 and group by
    ss

---
+++ count.sql
@@ -0,0 +1,24 @@
+select * from persons;
+
+
+
+select city,personid from persons order by personid desc;
+
+
+select * from persons;
+
+update persons set city='hjj' where personid='5';
+
+select count( personid),city from persons group by city;
+
+select count(0) from persons;
+select count(13) from persons;
+select count(*) from persons;
+
+
+
+
+
+
+
+
